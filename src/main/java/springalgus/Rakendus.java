acbdb7e9ed3e3e52a6db9f6453eb1accfce8610b
j71m <23724760+J71m@users.noreply.github.com>
2018-03-05 10:28:15 +0200

    korrutis

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -21,6 +21,10 @@ public class Rakendus {
     String rootFolder(String eesnimi){
         return "Root page!";
     }
+    @RequestMapping("/korrutis")
+    int rootFolder(int arv1, int arv2){
+        return arv1 * arv2;
+    }
  
     public static void main(String[] args) {
 		//System.getProperties().put("server.port", 40305);
