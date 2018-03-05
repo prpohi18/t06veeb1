c965e5d63ec4fa80b6044c4e364d6a95c105b594
Kädi Soodla <ksoodla@greeny.cs.tlu.ee>
2018-03-05 10:45:12 +0200

    korrutustehe

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -17,8 +17,13 @@ public class Rakendus {
         return "Tere, "+eesnimi;
     }
  
+	@RequestMapping("/korrutus")
+	int korruramine(int arv1, int arv2){
+		return arv1*arv2;
+	}
+ 
     public static void main(String[] args) {
-		//System.getProperties().put("server.port", 40305);
+		//System.getProperties().put("server.port", 40205);
         SpringApplication.run(Rakendus.class, args);
     }
 }
