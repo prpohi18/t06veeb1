ea99457a5206e254b36c9c2478bc9e80fbe63f11
Simone Niinemägi <31885412+niinsimo@users.noreply.github.com>
2018-03-05 10:40:58 +0200

    korrutus
    .

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -11,14 +11,21 @@ public class Rakendus {
     String tervitusfunktsioon() {
         return "Ahoi!";
     }
-    
-    @RequestMapping("/tervitus")
-    String tervitus2(String eesnimi){
-        return "Tere, "+eesnimi;
-    }
- 
+	@RequestMappping("/tervitus")
+	String tervitus2(String eesnimi) {
+		return "Tere, "+eesnimi; 
+	}
+	
+	@RequestMapping("/korrutus")
+	int korrutamine(int arv, int arv2){
+		return arv1*arv2;
+	}
     public static void main(String[] args) {
-		//System.getProperties().put("server.port", 4466);
+		//System.getProperties().put("server.port", 4488);
         SpringApplication.run(Rakendus.class, args);
     }
 }
+
+//scl enable rh-maven33 bash
+//mvn package
+//java -jar target/boot3-1.0-SNAPSHOT.jar
