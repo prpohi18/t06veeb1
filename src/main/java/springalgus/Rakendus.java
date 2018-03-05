fcf21a959853e5ea6af99b42414028276f704096
Dennis Richard Šulga <desulg@users.noreply.github.com>
2018-03-05 10:44:07 +0200

    Korrutus lisatud

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -1,4 +1,4 @@
-package springalgus;
+package dennis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,14 +11,24 @@ public class Rakendus {
     String tervitusfunktsioon() {
         return "Ahoi!";
     }
-    
     @RequestMapping("/tervitus")
     String tervitus2(String eesnimi){
-        return "Tere, "+eesnimi;
+        return "Tere, " + eesnimi;
+    }
+
+    @RequestMapping("/korrutus")
+    int korrutamine(int arv1, int arv2){
+        return arv1*arv2;  
     }
  
     public static void main(String[] args) {
-		//System.getProperties().put("server.port", 40305);
+		//System.getProperties().put("server.port", 40408);
         SpringApplication.run(Rakendus.class, args);
     }
 }
+
+//scl enable rh-maven33 bash
+//mvn package
+//java -jar target/boot3-1.0-SNAPSHOT.jar
+
+//java -jar -Dserver.port=40408 target/boot3-1.0-SNAPSHOT.jar
