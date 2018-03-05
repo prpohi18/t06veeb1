95c77a11a0bddd06a8e148bedca748b7d05e3b45
Mihkel Mägi <31948822+mihkel26@users.noreply.github.com>
2018-03-05 10:47:25 +0200

    multiply

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -20,7 +20,7 @@ public class Rakendus {
 	@RequestMapping("/multiply")
 	int multiply(int value1, int value2){
 		return value1*value2;
-	}
+	} 
  
     public static void main(String[] args) {
 		//System.getProperties().put("server.port", 40305);
