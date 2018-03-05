7ce89ea956bbc864c20934e99d15b0a05c45f920
Simone Niinemägi <31885412+niinsimo@users.noreply.github.com>
2018-03-05 11:04:12 +0200

    kirjeldus
    -

---
+++ tund6/src/main/java/springalgus/Rakendus.java
@@ -0,0 +1,31 @@
+package springalgus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@SpringBootApplication
+public class Rakendus {
+	@RequestMapping("/algus")
+    String tervitusfunktsioon() {
+        return "Ahoi!";
+    }
+	@RequestMappping("/tervitus")
+	String tervitus2(String eesnimi) {
+		return "Tere, "+eesnimi; 
+	}
+	
+	@RequestMapping("/korrutus")
+	int korrutamine(int arv, int arv2){
+		return arv1*arv2;
+	}
+    public static void main(String[] args) {
+		//System.getProperties().put("server.port", 4488);
+        SpringApplication.run(Rakendus.class, args);
+    }
+}
+
+//scl enable rh-maven33 bash
+//mvn package
+//java -jar target/boot3-1.0-SNAPSHOT.jar
