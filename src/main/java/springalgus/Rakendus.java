d0f6063c9c0c5c84ff1620017525cf5bd7049f8f
Kertu Kipper <phkel@users.noreply.github.com>
2018-03-06 09:35:41 +0200

    Add resources

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -4,21 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping; 
 import org.springframework.web.bind.annotation.RestController;
-@RestController
-@SpringBootApplication
-public class Rakendus {
-	@RequestMapping("/algus")
-    String tervitusfunktsioon() {
-        return "Ahoi!";
-    }
-    
-    @RequestMapping("/tervitus")
-    String tervitus2(String eesnimi){
-        return "Tere, "+eesnimi;
-    }
- 
-    public static void main(String[] args) {
-		//System.getProperties().put("server.port", 40305);
-        SpringApplication.run(Rakendus.class, args);
-    }
+  @RestController
+  @SpringBootApplication
+  public class Rakendus {
+  @RequestMapping("/algus")
+  String tervitusfunktsioon() {
+      return "hello";
+  }
+
+  @RequestMapping("/tervitus")
+  String tervitus2(String eesnimi){
+      return "Tere, "+eesnimi;
+  }
+
+  @RequestMapping("/korrutus")
+  int korrutamine(int arv1, int arv2){
+      return arv1*arv2;
+  }
+
+  public static void main(String[] args) {
+      //System.getProperties().put("server.port", 40305);
+      SpringApplication.run(Rakendus.class, args);
+  }
 }
