acc6d8ce2ab6c2e39aa57d9dcef4dc77d0b24036
Konstantin Blinkov <kostjaki@users.noreply.github.com>
2018-03-06 11:21:09 +0200

    töö3

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -3,6 +3,8 @@ package springalgus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.PostMapping; 
+import org.springframework.web.bind.annotation.PathVariable; 
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
@@ -17,11 +19,13 @@ public class Rakendus {
         return "Tere, "+eesnimi;
     }
     
-    @RequestMapping("/korrutus")
-    int korrutamine(int arv1, int arv2){
-        return arv1*arv2;
+    @RequestMapping("/korrutus/{arv1}/{arv2}")
+    String korrutamine(@PathVariable String arv1, @PathVariable String arv2){
+        if(arv1==null){return "esimene arv puudub";}
+        int vastus=Integer.parseInt(arv1)*Integer.parseInt(arv2);
+        return String.valueOf(vastus);
     }
-   //http://greeny.cs.tlu.ee:40376/korrutus?arv1=4&arv2=7
+   //http://greeny.cs.tlu.ee:40376/korrutus/4/7
    
     public static void main(String[] args) {
 		//System.getProperties().put("server.port", 40305);
