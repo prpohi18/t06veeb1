f883cb3bd580c03540eeae73e30f7f15e4babdc6
Egert Piksar <pikseger@greeny.cs.tlu.ee>
2018-03-06 09:32:54 +0200

    boot1

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -4,21 +4,30 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping; 
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @SpringBootApplication
+
 public class Rakendus {
 	@RequestMapping("/algus")
     String tervitusfunktsioon() {
-        return "Ahoi!";
+        return "Teretere!";
     }
-    
+	//http://greeny.cs.tlu.ee:4044/tervitus?eesnimi=juku
     @RequestMapping("/tervitus")
     String tervitus2(String eesnimi){
         return "Tere, "+eesnimi;
     }
- 
+	
+	//http://greeny.cs.tlu.ee:4044/korrutis?arv1=3&arv2=4
+	@RequestMapping("/korrutis")
+	int korrutamine(int arv1, int arv2){
+		return arv1*arv2;
+	}
+	
+	
     public static void main(String[] args) {
-		//System.getProperties().put("server.port", 40305);
+		//System.getProperties().put("server.port", 4044); MUL ON PORT 4044!!
         SpringApplication.run(Rakendus.class, args);
     }
 }
