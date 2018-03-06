c633deba4e9e42ed5380c6527dcfd9eb464727ef
Konstantin Blinkov <kostjaki@users.noreply.github.com>
2018-03-06 09:40:24 +0200

    töö

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -9,16 +9,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class Rakendus {
 	@RequestMapping("/algus")
     String tervitusfunktsioon() {
-        return "Ahoi!";
+        return "Ahoi poeg!";
     }
     
     @RequestMapping("/tervitus")
     String tervitus2(String eesnimi){
         return "Tere, "+eesnimi;
     }
+	
+	@RequestMapping("/arv")
+    int tervitus2(int arv1, int arv2){
+        return arv1*arv2;
+    }
+	
  
     public static void main(String[] args) {
 		//System.getProperties().put("server.port", 40305);
         SpringApplication.run(Rakendus.class, args);
     }
 }
+//mvn package
+//java -jar -Dserver.port=55557 target/boot3-1.0-SNAPSHOT.jar
+//http://greeny.cs.tlu.ee:55557/arv?arv1=6&arv2=8
