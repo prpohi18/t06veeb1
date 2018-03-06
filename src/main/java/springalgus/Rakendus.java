1d826c8c04e09da3584571c60afb2ad166b61907
Valeri Valentinov <valevale@greeny.cs.tlu.ee>
2018-03-06 09:33:01 +0200

    selgitus

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -9,13 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class Rakendus {
 	@RequestMapping("/algus")
     String tervitusfunktsioon() {
-        return "Ahoi!";
+        return "Hello";
     }
     
     @RequestMapping("/tervitus")
     String tervitus2(String eesnimi){
         return "Tere, "+eesnimi;
     }
+	
+	@RequestMapping("/korrutus")
+	int korrutamine(int arv1, int arv2){
+		return arv1*arv2;
+	}
  
     public static void main(String[] args) {
 		//System.getProperties().put("server.port", 40305);
