0833d6424ee2bcf571e34549d7c22fbd9a85b226
Grete Ojavere <GreteO@users.noreply.github.com>
2018-03-06 09:36:58 +0200

    Algus

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -9,13 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class Rakendus {
 	@RequestMapping("/algus")
     String tervitusfunktsioon() {
-        return "Ahoi!";
+        return "Hommikust!";
     }
     
     @RequestMapping("/tervitus")
     String tervitus2(String eesnimi){
         return "Tere, "+eesnimi;
-    }
+    } //http://greeny.cs.tlu.ee:36770/tervitus?eesnimi=Grete
+	
+	@RequestMapping("/korrutus")
+	int korrutamine(int arv1, int arv2){
+		return arv1 * arv2;
+	} //http://greeny.cs.tlu.ee:36770/korrutus?arv1=4&arv2=7
  
     public static void main(String[] args) {
 		//System.getProperties().put("server.port", 40305);
