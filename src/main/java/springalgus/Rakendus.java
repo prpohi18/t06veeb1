f0b9734fa5efcb1c72a366029a31eff5756d7703
Mihkel Haava <mhaava@tlu.ee>
2018-03-06 09:33:11 +0200

    server

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -1,4 +1,4 @@
-package springalgus;
+ package springalgus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,14 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class Rakendus {
 	@RequestMapping("/algus")
     String tervitusfunktsioon() {
-        return "Ahoi!";
+        return "<h1>Ahoi!</h1>";
     }
     
     @RequestMapping("/tervitus")
     String tervitus2(String eesnimi){
         return "Tere, "+eesnimi;
     }
- 
+	
+	@RequestMapping("/korrutus")
+	int korrutamine(int arv1, int arv2){
+		return arv1*arv2;
+	}
+	
     public static void main(String[] args) {
 		//System.getProperties().put("server.port", 40305);
         SpringApplication.run(Rakendus.class, args);
