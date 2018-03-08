0e3639ae80726b7b5254d9da39132a1cf2f53713
if17 <if17@A406-9.INFORMAATIKA>
2018-03-08 08:55:51 +0200

    Korrutamine lisatud

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -9,12 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class Rakendus {
 	@RequestMapping("/algus")
     String tervitusfunktsioon() {
-        return "Ahoi!";
+        return "Ahoi!!";
     }
     
     @RequestMapping("/tervitus")
     String tervitus2(String eesnimi){
         return "Tere, "+eesnimi;
+    }
+    
+    @RequestMapping("/korrutus")
+    int korrutamine(int arv1, int arv2){
+        return arv1*arv2;
     }
  
     public static void main(String[] args) {
