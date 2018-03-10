78b5f5d6fc38ffd410a3abd1ecdf24f7abb6fa56
Simone Niinemägi <31885412+niinsimo@users.noreply.github.com>
2018-03-10 21:48:21 +0200

    ruutvõrrand
    .

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -21,6 +21,19 @@ public class Rakendus {
 		int korrutamine(int arv1, int arv2){
 			return arv1*arv2;
 		}
+	
+	@RequestMapping("/ruutvorrand")
+		int lahendamine(int a, int b, int c){
+			String vastus;
+			if(a==0 || b==0 || c==0){
+				vastus = "Lahend puudub, sisend ei tohi olla null";
+			}else{
+				int x1 = (-b + Math.sqrt(Math.pow(b, 2) - (4 * a * c)) ) / (2 * a);
+				int x2 = (-b - Math.sqrt(Math.pow(b, 2) - (4 * a * c)) ) / (2 * a);
+				vastus = ("X1 = ", x1", X2 = ", x2);
+			}
+			return vastus;
+		}
 		
     public static void main(String[] args) {
 		//System.getProperties().put("server.port", 40305);
