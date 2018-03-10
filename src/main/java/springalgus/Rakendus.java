a823cf5a934aec121ff153677d75c68283d7c266
Simone Niinemägi <31885412+niinsimo@users.noreply.github.com>
2018-03-10 21:26:16 +0200

    test
    .

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -11,31 +11,14 @@ public class Rakendus {
     String tervitusfunktsioon() {
         return "Ahoi!";
     }
-     
+    
     @RequestMapping("/tervitus")
     String tervitus2(String eesnimi){
         return "Tere, "+eesnimi;
     }
-    
-    @RequestMapping("/korrutus")
-    int korrutamine(int arv1, int arv2){
-        return arv1*arv2;
-    }
-	
-	@RequestMapping("/ruutvorrand")
-	int lahendamine(int a, int b, int c){
-		int x1 = (-b + Math.sqrt(Math.pow(b, 2) - (4 * a * c)) ) / (2 * a);
-		int x2 = (-b - Math.sqrt(Math.pow(b, 2) - (4 * a * c)) ) / (2 * a);
-		String vastus = ("X1 = ", x1", X2 = ", x2);
-		return vastus;
-	}
-	
+ 
     public static void main(String[] args) {
-		System.getProperties().put("server.port", 4488);
+		//System.getProperties().put("server.port", 40305);
         SpringApplication.run(Rakendus.class, args);
     }
 }
-
-//scl enable rh-maven33 bash
-//mvn package
-//java -jar target/boot3-1.0-SNAPSHOT.jar
