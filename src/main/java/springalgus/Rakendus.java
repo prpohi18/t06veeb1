2494d7ebc8d8a1985f3696e82617c76575fc2d97
Simone Niinemägi <31885412+niinsimo@users.noreply.github.com>
2018-03-11 01:20:31 +0200

    kodutöö
    .

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -26,11 +26,11 @@ public class Rakendus {
 		int lahendamine(int a, int b, int c){
 			String vastus;
 			if(a==0 or b==0 or c==0){
-				vastus = ("Lahend puudub, sisend ei tohi olla null");
+				vastus = "Lahend puudub, sisend ei tohi olla null";
 			}else{
 				int x1 = (-b + Math.sqrt(Math.pow(b, 2) - (4 * a * c)) ) / (2 * a);
 				int x2 = (-b - Math.sqrt(Math.pow(b, 2) - (4 * a * c)) ) / (2 * a);
-				vastus = ("X1 = "+ x1", X2 = "+ x2);
+				vastus = ("X1 = "+x1+", X2 = "+ x2);
 			}
 			return vastus;
 		}
