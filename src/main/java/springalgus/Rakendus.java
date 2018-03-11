e883192a990d4af7a2c703bcdbe4dab55d8c46b8
Riho Noormets <noorriho@greeny.cs.tlu.ee>
2018-03-11 19:33:10 +0200

    Final

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -1,4 +1,4 @@
-package springalgus;
+package springAlgus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,19 +11,35 @@ public class Rakendus {
     String tervitusfunktsioon() {
         return "Ahoi!";
     }
-    
-    @RequestMapping("/tervitus")
-    String tervitus2(String eesnimi){
-        return "Tere, "+eesnimi;
-    }
-	@RequestMapping("/korrutus")
-	int korrutamine(int arv1, int arv2){
-		return arv1*arv2;
+	@RequestMapping("/tervitus")
+	String tervitus2(String eesnimi){
+		return "Tere, "+eesnimi;
 	}
+	 @RequestMapping("/korrutus")
+    int korrutamine(int arv1, int arv2){
+        return arv1*arv2;
+    }
+	
+	@RequestMapping("/ruutvorrand")
+		String lahendamine(double a, double b, double c){
+			double d = (Math.pow(b, 2) - (4 * a * c));
+			double x1, x2;
+			if (d >= 0) {
+				x1 = (-b + Math.sqrt(d) ) / (2 * a);
+				x2 = (-b - Math.sqrt(d) ) / (2 * a);
+				return "X1= "+x1+"X2= "+x2;
+			} else {
+				return "Lahendid puuduvad";
+			}
+		}
+	
  
     public static void main(String[] args) {
-		//System.getProperties().put("server.port", 40305);
+		System.getProperties().put("server.port", 40328);
         SpringApplication.run(Rakendus.class, args);
     }
 }
-//java -jar -Dserver.port=40328 target/boot3-1.0-SNAPSHOT.jar
+
+//scl enable rh-maven33 bash
+//mvn package
+//java -jar target/boot3-1.0-SNAPSHOT.jar
