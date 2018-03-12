1cf0ae559179cb4c426d32dc738a00e6d485c5db
Sten Piirsalu <piirsalusten@user.noreply.github.com>
2018-03-13 00:40:02 +0200

    taiendus

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -1,5 +1,5 @@
 package springalgus;
-
+import java.lang.Math;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping; 
@@ -7,15 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class Rakendus {
-	@RequestMapping("/arvutaPindala")
-    String pindalafunktsioon(double value1, double value2, double value3) {
+	@RequestMapping("/surface")
+    String surface(double value1, double value2, double value3) {
         if(value1 + value2 > value3 & value1 + value3 > value2 & value2 + value3 > value1){
-			double pindala = (value1 * value2 * value3)/2;
-			pindalaFunktsioon = pindala;
+			double p = (value1 + value2 + value3)/2;
+			double area = Math.sqrt(p*(p-value1)*(p-value2)*(p-value3));
+			String surface = String.valueOf(area);
 		}else{
-			pindalafunktsioon = "Ei ole kolmnurk!";
+			surface = "Ei ole kolmnurk!";
 		}
-		return "Kolmnurga pindala on "+ pindalaFunktsioon;
+		return "Kolmnurga pindala on "+ surface;
     }
    
     public static void main(String[] args) {
