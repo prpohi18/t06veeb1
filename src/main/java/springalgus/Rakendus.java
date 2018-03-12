4131fb294eb4e23c97fad51d41e4cbb59604394a
Sten Piirsalu <piirsalusten@user.noreply.github.com>
2018-03-12 23:18:10 +0200

    parandus rakendus.java failis

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -9,17 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class Rakendus {
 	@RequestMapping("/arvutaPindala")
     String pindalafunktsioon(double value1, double value2, double value3) {
-        if value1 + value2 > value3 & value1 + value3 > value2 & value2 + value3 > value1{
+        if(value1 + value2 > value3 & value1 + value3 > value2 & value2 + value3 > value1){
 			double pindala = (value1 * value2 * value3)/2;
 			pindalaFunktsioon = pindala;
-		else {
+		}else{
 			pindalafunktsioon = "Ei ole kolmnurk!";
 		}
 		return "Kolmnurga pindala on "+ pindalaFunktsioon;
     }
-	}
    
- 
     public static void main(String[] args) {
 		//System.getProperties().put("server.port", 40305);
         SpringApplication.run(Rakendus.class, args);
