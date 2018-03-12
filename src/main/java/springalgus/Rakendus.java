71721a4f26844cbf8665d114cef6cac25e17ce1b
Sten Piirsalu <piirsalusten@user.noreply.github.com>
2018-03-12 10:23:25 +0200

    lisatud

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -7,15 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class Rakendus {
-	@RequestMapping("/algus")
-    String tervitusfunktsioon() {
-        return "Ahoi!";
+	@RequestMapping("/arvutaPindala")
+    String pindalafunktsioon(double value1, double value2, double value3) {
+        if value1 + value2 > value3 & value1 + value3 > value2 & value2 + value3 > value1{
+			double pindala = (value1 * value2 * value3)/2;
+			pindalaFunktsioon = pindala;
+		else {
+			pindalafunktsioon = "Ei ole kolmnurk!";
+		}
+		return "Kolmnurga pindala on "+ pindalaFunktsioon;
     }
-    
-    @RequestMapping("/tervitus")
-    String tervitus2(String eesnimi){
-        return "Tere, "+eesnimi;
-    }
+	}
+   
  
     public static void main(String[] args) {
 		//System.getProperties().put("server.port", 40305);
