84aafce9906cb45439ab3d105df97521ee3a249b
Sten Piirsalu <piirsalusten@user.noreply.github.com>
2018-03-13 00:43:55 +0200

    taiendus

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -9,10 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class Rakendus {
 	@RequestMapping("/surface")
     String surface(double value1, double value2, double value3) {
-        if(value1 + value2 > value3 & value1 + value3 > value2 & value2 + value3 > value1){
+		String surface = "";
+		if(value1 + value2 > value3 & value1 + value3 > value2 & value2 + value3 > value1){
 			double p = (value1 + value2 + value3)/2;
 			double area = Math.sqrt(p*(p-value1)*(p-value2)*(p-value3));
-			String surface = String.valueOf(area);
+			surface = String.valueOf(area);
 		}else{
 			surface = "Ei ole kolmnurk!";
 		}
