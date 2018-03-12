df4f575fc836c1a3a775f24eafecd17c6d69c44a
Peeter <peeterr@tlu.ee>
2018-03-12 09:07:31 +0200

    kilovattide arvutus

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -21,6 +21,11 @@ public class Rakendus {
 		int korrutamine(int arv1, int arv2){
 		return arv1 * arv2;
 	}
+	
+	@RequestMapping("/kilovatt")
+		double kilowatt(int arv1){
+		return arv1 * 0.745699872;
+	}
  
     public static void main(String[] args) {
 		//System.getProperties().put("server.port", 40305);
