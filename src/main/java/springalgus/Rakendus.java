d1105808a6ba55a87268175f6e20edbe209b5945
if17 <if17@A406-25.INFORMAATIKA>
2018-03-15 12:28:46 +0200

    Kodutoo6

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -4,21 +4,28 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping; 
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @SpringBootApplication
-public class Rakendus {
-	@RequestMapping("/algus")
-    String tervitusfunktsioon() {
-        return "Ahoi!";
-    }
-    
-    @RequestMapping("/tervitus")
-    String tervitus2(String eesnimi){
-        return "Tere, "+eesnimi;
-    }
+public class Rakendus{	
+    @RequestMapping("/")
+	public String tervitus(){
+		return "Tere";
+	}
+
+	@RequestMapping("/geomeetriline_keskmine")
+	public String geomkesk(String arv1, String arv2){
+	  try{
+		double a1=Double.parseDouble(arv1);
+		double a2=Double.parseDouble(arv2);
+		return String.valueOf(Math.sqrt(a1*a2));
+	  } catch(Exception e){
+		 return "Viga: "+e.getMessage();
+	  }
+	}
  
-    public static void main(String[] args) {
-		//System.getProperties().put("server.port", 40305);
+	public static void main(String[] args){
+		//System.getProperties().put("server.port", 4200);
         SpringApplication.run(Rakendus.class, args);
-    }
+	}
 }
