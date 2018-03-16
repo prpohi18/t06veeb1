53959eb6056263bb5c6e0b2c2afbb67ab774562b
Kärt Nigols <kart.nigols@gmail.com>
2018-03-16 17:19:11 +0200

    kodutöö

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -7,18 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class Rakendus {
-	@RequestMapping("/algus")
-    String tervitusfunktsioon() {
-        return "Ahoi!";
+    @RequestMapping("/ruutvorrand")
+    String korrutamine(int arv1, int arv2){
+        String vastus = "vastus";
+        int vaartus = (int) ((arv1*arv2)/2);
+            return vastus;
+        
+            
     }
     
-    @RequestMapping("/tervitus")
-    String tervitus2(String eesnimi){
-        return "Tere, "+eesnimi;
-    }
- 
+    
     public static void main(String[] args) {
-		//System.getProperties().put("server.port", 40305);
         SpringApplication.run(Rakendus.class, args);
     }
+    
 }
