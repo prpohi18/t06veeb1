ef7b0fd81121e89fa00a402bad0788b9b90e4570
Kärt Nigols <kart.nigols@gmail.com>
2018-03-16 17:41:04 +0200

    kodutöö

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -1,4 +1,5 @@
 package springalgus;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping; 
@@ -7,16 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 
 public class Rakendus {
-    @RequestMapping("/ruutvorrand")
+    @RequestMapping("/kolmnurk")
     String korrutamine(int arv1, int arv2){
         String vastus = "vastus";
         int vaartus = (int) ((arv1*arv2)/2);
-            return vastus;
-        
-            
+        vastus = "Vastus on:"+ vaartus;
+            return vastus; 
     }
-    
-    
+
     public static void main(String[] args) {
         SpringApplication.run(Rakendus.class, args);
     }
