706ddae46172f713371f176279c5875e4f6a5c81
Kevin <Kevin@KOKSEN>
2018-03-20 18:38:28 +0200

    Kodutoo6

--- src/main/java/springalgus/Rakendus6.java
+++ src/main/java/springalgus/Rakendus6.java
@@ -13,7 +13,7 @@ public class Rakendus6 {
     String korrutamine(int arv1, int arv2) {
         String vastus;
         int vaartus;
-        vaartus = arv1/(arv2*arv2);
+        vaartus = arv1/((arv2/100)*(arv2/100));
         vastus = "Vastus on: " + vaartus;
             return vastus;
     }
