7168d0d4f2995193f4f3814cc0add290f3549475
Kevin <Kevin@KOKSEN>
2018-03-20 19:02:56 +0200

    Kodutoo6

--- src/main/java/springalgus/Rakendus6.java
+++ src/main/java/springalgus/Rakendus6.java
@@ -12,8 +12,7 @@ public class Rakendus6 {
     @RequestMapping("/kehamassiindeks")
     String korrutamine(int arv1, int arv2) {
         String vastus;
-        int vaartus;
-        vaartus = (arv1 + arv2);
+        double vaartus = (arv1 + arv2/100);
         vastus = "Vastus on: " + vaartus;
             return vastus;
     }
