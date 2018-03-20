88b852656514ad36cd2f48a5043734f872ead69a
Kevin <Kevin@KOKSEN>
2018-03-20 17:34:55 +0200

    Kodutoo6

---
+++ src/main/java/springalgus/Rakendus6.java
@@ -0,0 +1,23 @@
+package springalgus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+
+public class Rakendus6 {
+    @RequestMapping("/kehamassiindeks")
+    String korrutamine(int arv1, int arv2) {
+        String vastus = "vastus";
+        int vaartus = (int) ((arv1*arv2)/2);
+        vastus = "Vastus on: " + vaartus;
+            return vastus;
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(Rakendus6.class, args);
+    }
+    
+}
