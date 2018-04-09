157778f4e0c991d884f5cd7ee9cd59a830ea5a44
Kristo Roots <rootkris@users.noreply.github.com>
2018-04-09 08:29:07 +0300

    tyhik

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -1,6 +1,6 @@
 package springalgus;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication; 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping; 
 import org.springframework.web.bind.annotation.RestController;
