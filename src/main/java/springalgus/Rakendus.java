2c1a2157a862fde6ee0a262758bbaa431c792145
Sten Piirsalu <piirsalusten@user.noreply.github.com>
2018-05-05 16:59:08 +0300

    error fix

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -5,6 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping; 
 import org.springframework.web.bind.annotation.RestController;
 @RestController
+public class IndexController implements ErrorController{
+
+  private static final String PATH = "/error";
+
+  @RequestMapping(value = PATH)
+  public String error() {
+      return "Error handling";
+  }
+
+  @Override
+  public String getErrorPath() {
+      return PATH;
+  }
+}
 @SpringBootApplication
 public class Rakendus {
 	@RequestMapping("/surface")
