662902b3e2c33b6503a02e28b5ccf28b1e5095ce
Hendrik Heinsar <heinhend@greeny.cs.tlu.ee>
2018-05-07 19:50:33 +0300

    first

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -17,7 +17,7 @@ public class Rakendus {
         return "Tere, "+eesnimi;
     }
     
-    @RequestMapping("/ristkylik")
+    @RequestMapping("/korrutamine")
        float korrutamine(float arv1, float arv2){
            return arv1*arv2;
        }
