99f4a3cd11d07d27e485dc960e5c30e3ade59cc6
Hendrik Heinsar <heinhend@greeny.cs.tlu.ee>
2018-05-07 19:33:31 +0300

    first

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -18,9 +18,9 @@ public class Rakendus {
     }
     
     @RequestMapping("/ristkylik")
-    +    float korrutamine(float arv1, float arv2){
-    +        return arv1*arv2;
-    +    }
+       float korrutamine(float arv1, float arv2){
+           return arv1*arv2;
+       }
 
     public static void main(String[] args) {
 		//System.getProperties().put("server.port", 40305);
