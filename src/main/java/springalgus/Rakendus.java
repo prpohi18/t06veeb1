cf2f05f51330b0c1ceee2acb5b5e5adc7451e2bb
Kaur K <kaurk@users.noreply.github.com>
2018-05-16 00:52:24 +0300

    HW6 finished - risttahuka ruumala kalkulaator Added online Heroku URL

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -1,19 +1,5 @@
-/*
-1) Spring Boot - repo (Jaagupilt failid (https://github.com/jaagup/t06veebneljapaev), siis tõmbad selle endale Netbeansi niing Run>Clean and Build
-2) Paned tööle ja siis brauseris aadressi ribale: 
-    http://localhost:8080/tervitus?eesnimi=Juku ning ütleb, Tere, Juku
-    http://localhost:8080/algus ning ütleb Ahoi!
-3) Netbeansi alt (Run(Rakendus)) ristist kinni ja uuesti RUN
-4) Herokusse konto ja sealt panid githubi repo kylge ning kuvasid repos olevat 
-    appi l2bi heroku. K2tte sain aadressilt: https://springalguskaur.herokuapp.com/algus
-5) Tegin HTML faili Netbeansis failide alla boot3>src>main> resources>public>kirjeldus.html 
-    ning brauseris sain selle k2tte aadressil http://localhost:8080/kirjeldus.html
-6) Jaagup tegi ylesande lahenduse.
+//  http://t06veeb1kaur.herokuapp.com/index.html
 
-
-
-
-*/
 package springalgus;
 
 import org.springframework.boot.SpringApplication;
