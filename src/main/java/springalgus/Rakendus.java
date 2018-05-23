2f1255dbebc2255ff5d6db34dbf479f11918747c
Caspar Sepp <31949001+casparsepp@users.noreply.github.com>
2018-05-23 22:55:46 +0300

    küsimärk

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -1,13 +1,14 @@
-package springalgus;
+/*package springalgus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping; 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;*/
+
 @RestController
 @SpringBootApplication
+
 public class Rakendus {
-	
 	@RequestMapping("/kmi")
 	String kmi(double weight, double length){
 		double bmi = Math.round(weight/(length*length) * 100.0) / 100.0;
@@ -25,7 +26,7 @@ public class Rakendus {
 	} 
  
     public static void main(String[] args) {
-		//System.getProperties().put("server.port", 40305);
+			System.getProperties().put("server.port", 40305);
         SpringApplication.run(Rakendus.class, args);
     }
 }
