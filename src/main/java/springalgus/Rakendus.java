98b34ea282c6efd2c5e11001331b95193d26c381
Konstnatin Blinkov <31948927+kostjaki@users.noreply.github.com>
2018-05-29 17:00:17 +0300

    lisamine

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -2,43 +2,37 @@ package springalgus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping; 
-import org.springframework.web.bind.annotation.PostMapping; 
-import org.springframework.web.bind.annotation.PathVariable; 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class Rakendus {
 	@RequestMapping("/algus")
     String tervitusfunktsioon() {
-        return "Hommikust!";
+        return "Hello";
     }
-    
-    @RequestMapping("/tervitus") //Get'iga tavaline küsimine
+
+    @RequestMapping("/tervitus")
     String tervitus2(String eesnimi){
         return "Tere, "+eesnimi;
-    } //http://greeny.cs.tlu.ee:36770/tervitus?eesnimi=Grete
-	
+    }
+
 	@RequestMapping("/korrutus/{arv1}/{arv2}")
 	String korrutamine(@PathVariable String arv1, @PathVariable String arv2){
-		if(arv1==null){return "esimene arv puudub";}
+		if(arv1==null){return "esimene arv puudu";}
 		int vastus=Integer.parseInt(arv1)*Integer.parseInt(arv2);
 		return String.valueOf(vastus);
-	} ////http://greeny.cs.tlu.ee:36770/korrutus/4/7
-		//http://greeny.cs.tlu.ee:36770/korrutus?arv1=4&arv2=7
-
-	//Kehamassiindeks:	
-	@RequestMapping("/kmindeks/{pikkus}/{kaal}")
-	String arvutamine(@PathVariable String pikkus, @PathVariable String kaal){
-		if(pikkus==null){return "arv puudub";}
-		double pikkusRuut=Math.pow((Double.parseDouble(pikkus)/100),2);
-		double kmi=(Integer.parseInt(kaal)*1.0) / pikkusRuut;
-		return String.valueOf(kmi);
 	}
-	//https://programmeerimine.herokuapp.com/kehamassindeks.html
-	
 
- 
+	@RequestMapping("/risttahukas/{arv1}/{arv2}/{arv3}")
+	String lahendamine(@PathVariable String arv1, @PathVariable String arv2, @PathVariable String arv3){
+		if(arv1==null){return "esimene arv puudu";}
+		int vastus1=2*(Integer.parseInt(arv1)*Integer.parseInt(arv2)+Integer.parseInt(arv1)*Integer.parseInt(arv3)+Integer.parseInt(arv2)*Integer.parseInt(arv3));
+		return String.valueOf(vastus1);
+	}
+
     public static void main(String[] args) {
 		//System.getProperties().put("server.port", 40305);
         SpringApplication.run(Rakendus.class, args);
