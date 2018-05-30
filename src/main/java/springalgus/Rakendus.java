e3fb52269e880c5d3aeb52c29a25953dfe73776d
Mihkel Jäe <31515689+jaaae@users.noreply.github.com>
2018-05-30 09:07:56 +0300

    kodutoo6

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -7,18 +7,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class Rakendus {
-	@RequestMapping("/algus")
-    String tervitusfunktsioon() {
-        return "Ahoi!";
+    @RequestMapping("/pythagoras")
+    double pyth(double kaatet1, double kaatet2){
+        double hüpotenuus= Math.sqrt(Math.pow(kaatet1, 2) + Math.pow(kaatet2, 2));
+        hüpotenuus = Math.round(hüpotenuus * 1000D) / 1000D;
+        return hüpotenuus;
     }
-    
-    @RequestMapping("/tervitus")
-    String tervitus2(String eesnimi){
-        return "Tere, "+eesnimi;
+
+    @RequestMapping("/pindala")
+    double pind(double kaatet1, double kaatet2){
+        double pindala= kaatet1 * kaatet2 / 2;
+        return pindala;
+    }
+
+    @RequestMapping("/ümbermõõt")
+    double ümber(double kaatet1, double kaatet2){
+        double hüpotenuus= Math.sqrt(Math.pow(kaatet1, 2) + Math.pow(kaatet2, 2));
+        hüpotenuus = Math.round(hüpotenuus * 1000D) / 1000D;
+        double ümbermõõt = hüpotenuus + kaatet1 + kaatet2;
+        return ümbermõõt;
     }
  
     public static void main(String[] args) {
-		//System.getProperties().put("server.port", 40305);
+		//System.getProperties().put("server.port", 40905);
         SpringApplication.run(Rakendus.class, args);
     }
 }
+
+//java -jar -Dserver.port=40910 target/boot3-1.0-SNAPSHOT.jar
