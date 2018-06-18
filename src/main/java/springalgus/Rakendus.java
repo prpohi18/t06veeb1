6846442faec90bb393520c210a1fc193b537baa8
Joosep Penjam <penjmart@greeny.cs.tlu.ee>
2018-06-18 16:07:06 +0300

    6. töö lisatud

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -3,8 +3,6 @@ package springalgus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping; 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
@@ -19,16 +17,23 @@ public class Rakendus {
         return "Tere, "+eesnimi;
     }
 	
-	@RequestMapping("/korrutus/{arv1}/{arv2}")
-	String korrutamine(@PathVariable String arv1, @PathVariable String arv2){
-		if (arv1==null){return "esimene arv puudub";}
-		int vastus=Integer.parseInt(arv1)*Integer.parseInt(arv2);
-		return String.valueOf(vastus);
+	@RequestMapping("/korrutus")
+	int korrutis(int arv1, int arv2) {
+		return arv1*arv2;
+	}
+		
+	@RequestMapping("/arvutus")
+	double arvutus(double reps, double weight) {
+		if (reps>1){
+			return (weight*(1+(reps/30)));
+		} else {
+			return 0;
+		}			
 	}
  
     public static void main(String[] args) {
-		//System.getProperties().put("server.port", 40305);
-        SpringApplication.run(Rakendus.class, args);
+		SpringApplication.run(Rakendus.class, args);
     }
 }
 
+//System.getProperties().put("server.port", 40355);
